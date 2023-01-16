442ecee3f6f4b50603d4e38e1c9607d36091f0db
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-16 14:30:42 -0300

    Complement previous commit

--- script-4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-]
-cp watermark_template.png watermark.png
-convert -size 511x81 \
-  \( xc:transparent -font "Arial" -pointsize 32 -fill black -draw "rotate -4 text 20,60 'ImageMagick'" -blur 0x1 \) \
-  -geometry +2+2   -composite \
-  -font "Arial" -fill white -pointsize 32 -draw "rotate -4 text 20,60 'ImageMagick'" \
-  watermark.png
-#- from https://stackoverflow.com/a/20861391/5623661
--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-convert -size 280x100 pattern:SMALLFISHSCALES \
-  \( xc:transparent -font "Menlo" -pointsize 32 -fill black -draw "rotate -4 text 20,60 'ImageMagick'" -blur 0x1 \) \
-  -geometry +2+2   -composite \
-  -font "Menlo" -fill white -pointsize 32 -draw "rotate -4 text 20,60 'ImageMagick'" \
-  watermark.png
-#- from https://stackoverflow.com/a/20861391/5623661
