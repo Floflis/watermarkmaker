4d84f9311556e29deb05512edc94809709a46fd2
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-16 15:47:06 -0300

    yep yip! now it generates a transparent image from zero, so removing image dependences and making this reproducible

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 . /usr/lib/floflis/./config
 
-cp ../watermark_template.png ../watermark.png
+if [ ! -f ../watermark_template.png ]; then convert -size 511x81 xc:transparent ../watermark_template.png; fi
 
-convert ../watermark.png -size 511x81 \
+convert ../watermark_template.png -size 511x81 \
   \( xc:transparent -font "Arial" -pointsize 20 -fill black -draw "text 40,35 '$osname $osversion Insider Preview'" -blur 0x1 \) \
   -geometry +2+2   -composite \
   -font "Arial" -fill white -pointsize 20 -draw "text 40,35 '$osname $osversion Insider Preview'" \
