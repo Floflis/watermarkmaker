21463f8fd8ed2e35ebb8845f3025a48c44aafcb6
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-22 03:45:47 -0300

    paliative solution: create watermark at root folder of this repo

--- run.sh
+++ run.sh
@@ -2,24 +2,24 @@
 
 . /usr/lib/floflis/./config
 
-if [ ! -f ../watermark_template.png ]; then convert -size 511x81 xc:transparent ../watermark_template.png; fi
+if [ ! -f watermark_template.png ]; then convert -size 511x81 xc:transparent watermark_template.png; fi
 #- from https://stackoverflow.com/a/39504523/5623661
 
-convert ../watermark_template.png -size 511x81 \
+convert watermark_template.png -size 511x81 \
   \( xc:transparent -font "Arial" -pointsize 20 -fill black -draw "text 40,35 '$osname $osversion Insider Preview'" -blur 0x1 \) \
   -geometry +2+2   -composite \
   -font "Arial" -fill white -pointsize 20 -draw "text 40,35 '$osname $osversion Insider Preview'" \
-  ../watermark.png
+  watermark.png
 #- from https://stackoverflow.com/a/20861391/5623661
 
-convert ../watermark.png -size 511x81 \
+convert watermark.png -size 511x81 \
   \( xc:transparent -font "Arial" -pointsize 20 -fill black -draw "text 40,60 'Evaluation compilation. Build $osbuild $osbuildcodename'" -blur 0x1 \) \
   -geometry +2+2   -composite \
   -font "Arial" -fill white -pointsize 20 -draw "text 40,60 'Evaluation compilation. Build $osbuild $osbuildcodename'" \
-  ../watermark.png
+  watermark.png
   
-convert ../watermark.png -size 511x81 \
+convert watermark.png -size 511x81 \
   \( xc:transparent -font "Arial" -pointsize 16 -fill black -draw "text 180,76 'Build attempt $specialbuildattempt'" -blur 0x1 \) \
   -geometry +2+2   -composite \
   -font "Arial" -fill white -pointsize 16 -draw "text 180,76 'Build attempt $specialbuildattempt'" \
-  ../watermark.png
+  watermark.png
